c3658ea734df2e245c254d4c9e97c06aca63d61f
leotanaka4 <89543577+leotanaka4@users.noreply.github.com>
2021-09-28 12:13:35 -0300

    Update Parte1.c

--- Parte1.c
+++ Parte1.c
@@ -1,24 +1,21 @@
-int usario();
+#include <stdio.h>
+
+#define MAX_LINHAS 6
+#define MAX_COLUNAS 5
 
 int main(){
-  int matrizA[6][5]={{0,0,1,0,0},{0,1,0,1,0},{1,0,0,0,1},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
-  int matrizB[6][5]={{1,1,1,1,0},{1,0,0,1,1},{1,1,1,1,0},{1,1,1,1,0},{1,0,0,1,1},{1,1,1,1,0}};
-  int matrizC[6][5]={{1,1,1,1,1},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,1,1,1,1}};
-  int matrizD[6][5]={{1,1,1,1,0},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,0}};
-  int matrizE[6][5]={{1,1,1,1,1},{1,0,0,0,0},{1,1,1,0,0},{1,1,1,0,0},{1,0,0,0,0},{1,1,1,1,1}};
+  int matrizA[MAX_LINHAS][MAX_COLUNAS]={{0,0,1,0,0},{0,1,0,1,0},{1,0,0,0,1},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
+  int matrizB[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,0},{1,0,0,1,1},{1,1,1,1,0},{1,1,1,1,0},{1,0,0,1,1},{1,1,1,1,0}};
+  int matrizC[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,0,0,0,0},{1,1,1,1,1}};
+  int matrizD[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,0},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,0}};
+  int matrizE[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,0},{1,1,1,0,0},{1,1,1,0,0},{1,0,0,0,0},{1,1,1,1,1}};
   int somaA=0, somaB=0, somaC=0, somaD=0, somaE=0;
-  int matrizUsuario[6][5];
-  matrizUsuario[6][5]=usuario();
-}
-
-int usuario(){
-  int i, j, matriz[6][5];
+  int i, j, matrizUsuario[MAX_LINHAS][MAX_COLUNAS];
   printf("WiSARD Original\n\nDigite uma letra em formato de uma matriz 6x5(1:Preto;0:Branco):\n\n");
-  for(i=0;i<6;i++) {
-    for(j=0;j<5;j++) {
-      printf("Digite a cor na linha %d e na coluna %d: ", i, j);
-      scanf("%d", &matriz[i][j]);
+  for(i=0;i<MAX_LINHAS;i++) {
+    for(j=0;j<MAX_COLUNAS;j++) {
+      printf("Digite a cor do pixel na linha %d e na coluna %d: ", i, j);
+      scanf("%d", &matrizUsuario[i][j]);
     }
   }
-  return matriz[6][5];
 }
