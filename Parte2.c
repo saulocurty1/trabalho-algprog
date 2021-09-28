ef97ae3b3c6f7d27284b00abb475c3c06f7789c6
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2021-09-28 13:03:55 -0300

    Update Parte2.c

--- Parte2.c
+++ Parte2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+#define MAX_LINHAS 6
+#define MAX_COLUNAS 5
+
+int main() {
+  int matrizA1[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizA2[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizA3[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizB1[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizB2[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizB3[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizC1[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizC2[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizC3[MAX_LINHAS][MAX_COLUNAS]=
+}
