8791a53aaee6f7113523ca4c6b0d6e79330d8c4b
Leonardo Tanaka <89543577+leotanaka4@users.noreply.github.com>
2021-10-01 08:07:06 -0300

    Update Parte2.c

--- Parte2.c
+++ Parte2.c
@@ -5,30 +5,30 @@
 
 int main() {
   int matrizA1[MAX_LINHAS][MAX_COLUNAS]={{0,1,1,1,0},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
-  int matrizA2[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizA3[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizA4[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizA5[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizA2[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
+  int matrizA3[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,0,1,0}};
+  int matrizA4[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,0,1,0,0},{0,1,0,1,0},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
+  int matrizA5[MAX_LINHAS][MAX_COLUNAS]={{0,0,1,0,0},{0,1,0,1,0},{1,0,0,0,1},{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1}};
   int matrizE1[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,0,0,0,0},{1,1,1,1,1}};
-  int matrizE2[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizE3[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizE4[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizE5[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizE2[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,0},{1,1,1,1,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,1,1,1}};
+  int matrizE3[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,0},{1,0,0,0,0},{1,1,1,0,0},{1,0,0,0,0},{1,1,1,1,0},{0,0,0,0,0}};
+  int matrizE4[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,1,0,0},{1,0,0,0,0},{1,1,1,1,0}};
+  int matrizE5[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,0},{1,0,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,1,1,1}};
   int matrizI1[MAX_LINHAS][MAX_COLUNAS]={{0,1,1,1,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,1,1,1,0}};
-  int matrizI2[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizI3[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizI4[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizI5[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizI2[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{1,1,1,1,1}};
+  int matrizI3[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,1,1,1,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,1,1,1,0}};
+  int matrizI4[MAX_LINHAS][MAX_COLUNAS]={{0,1,1,1,0},{0,0,1,0,0},{0,0,1,0,0},{0,0,1,0,0},{0,1,1,1,0},{0,0,0,0,0}};
+  int matrizI5[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,1,1,1,0},{0,0,1,0,0},{0,0,1,0,0},{0,1,1,1,0},{0,0,0,0,0}};
   int matrizO1[MAX_LINHAS][MAX_COLUNAS]={{0,1,1,1,0},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{0,1,1,1,0}};
-  int matrizO2[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizO3[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizO4[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizO5[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizO2[MAX_LINHAS][MAX_COLUNAS]={{1,1,1,1,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,1}};
+  int matrizO3[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,0,0}};
+  int matrizO4[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,0,0,0,0},{0,1,1,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0}};
+  int matrizO5[MAX_LINHAS][MAX_COLUNAS]={{0,1,1,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,0,0},{0,0,0,0,0}};
   int matrizU1[MAX_LINHAS][MAX_COLUNAS]={{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{0,1,1,1,0}};
-  int matrizU2[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizU3[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizU4[MAX_LINHAS][MAX_COLUNAS]=
-  int matrizU5[MAX_LINHAS][MAX_COLUNAS]=
+  int matrizU2[MAX_LINHAS][MAX_COLUNAS]={{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,0,0,0,1},{1,1,1,1,1}};
+  int matrizU3[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,0,0}};
+  int matrizU4[MAX_LINHAS][MAX_COLUNAS]={{0,0,0,0,0},{0,0,0,0,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0}};
+  int matrizU5[MAX_LINHAS][MAX_COLUNAS]={{0,1,0,1,0},{0,1,0,1,0},{0,1,0,1,0},{0,1,1,1,0},{0,0,0,0,0},{0,0,0,0,0}};
   int i, j, matrizUsuario[MAX_LINHAS][MAX_COLUNAS];
   printf("WiSARD com Bleaching\n\nDigite uma letra em formato de uma matriz 6x5(1:Preto;0:Branco):\n\n");
   for(i=0;i<MAX_LINHAS;i++) {
