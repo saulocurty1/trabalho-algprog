ea75eb260ef39479fd7a65b76532aa377b2db614
Saulo Curty <sullo152@gmail.com>
2021-10-07 21:38:43 -0300

    Neuro.c

---
+++ Neuro.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int neuA[30] = {0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1,1,0,0,0,1,1,0,0,0,1};
+int neuB[30];
+int neuC[30];
+int neuD[30];
+int neuE[30];
+int neuF[30];
+int neuG[30];
+int neuH[30];
+int neuI[30];
+int neuJ[30];
+int neuL[30];
+int neuM[30];
+int neuN[30];
+int neuO[30];
+int neuP[30];
+int neuQ[30];
+int neuR[30];
+int neuS[30];
+int neuT[30];
+int neuU[30];
+int neuV[30];
+int neuX[30];
+int neuZ[30];
+
+void CriaDiscriminante(int *neuronio, int *tupla){
+    int contador = 0;
+    int a, b, c = 0;
+    int valor = 0;
+    for(int i = 0; i < 30; i = i + 3){
+        a = tupla[i];
+        b = tupla[i + 1];
+        c = tupla[i + 2];
+        valor  = (pow(2, 0) * a) + (pow(2, 1) * b) + (pow(2, 2) * c) + contador;  
+        contador += 8;
+        neuronio[valor]++;
+    }
+
+    for (int  i = 0; i < 30; i++)
+    {
+        printf("%d", neuronio[i]);
+    }
+    
+}
+
+
+
+int main(){
+
+}
