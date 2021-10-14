b561c6bbdfe61edaa828681a86a34e94717bbab6
Saulo Curty <48524825+saulocurty1@users.noreply.github.com>
2021-10-14 01:06:54 -0300

    Create funcoes.c

---
+++ bleachingSI/funcoes.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <math.h>
+
+    
+
+void geraTuplas(int *vetorTuplas, int tamanho){
+    //Variaveis locais
+    int i;
+    
+    //Gerando o vetor de coordenadas com base na quantidade de posições
+    int coordenadas[tamanho];
+    for(i=0; i<tamanho; i++){
+        coordenadas[i] = i;
+    }
+
+    //Embaralhando o vetor de coordenadas
+    embaralhar(coordenadas, tamanho);
+
+    //Atribuindo os valores das coordenadas ao vetor de tuplas
+    for(i=0; i<tamanho; i++)
+        vetorTuplas[i] = coordenadas[i];
+}
+
+void embaralhar(int *vet, int vetSize){
+    int i;
+	for (i = 0; i < vetSize; i++)
+	{
+		int r = rand() % vetSize;
+		int temp = vet[i];
+		vet[i] = vet[r];
+		vet[r] = temp;
+}}
+
+void posicaoNeuronios(int *vetorTuplas, int *vetorLetras, int *vetorPosicao, int tamanho){
+    int i;
+
+    //Compara se a letra possui 0 ou 1 na posição da tupla e atribui o mesmo valor ao vetor de posições
+    for(i=0; i<tamanho; i++){
+        if(vetorLetras[vetorTuplas[i]] == 0){
+            vetorPosicao[i] = 0;
+        }
+        else{
+            vetorPosicao[i] = 1;
+        } 
+    }
+}
+
+
+void Zera(int *ma, int tamanho){
+    //Transforma um vetor em um vetor de zeros
+    for(int i=0; i< tamanho; i++){
+        ma[i] = 0;
+    }
+}
+
+void CriaDiscriminante(int *neuronio, int *posicoes){
+    int contador = 0;
+    int a, b, c = 0;
+    int valor = 0;
+    for(int i = 0; i < 30; i = i + 3){
+        a = posicoes[i];
+        b = posicoes[i + 1];
+        c = posicoes[i + 2];
+        //COnvertendo de binario pra decimal para pegar a posição do vetor
+        valor  = (pow(2, 0) * c) + (pow(2, 1) * b) + (pow(2, 2) * a) + contador;  
+        contador += 8;
+        neuronio[valor]++;
+    }   
+}
+
+int comparadisc(int neuroletra[], int neuroalfabeto[]){
+    //Compara duas discriminantes
+    int i;
+    int contadorigualdade=0;
+    for(i=0;i<80;i++){
+        if(neuroletra[i]>0 && neuroalfabeto[i]>0){
+            contadorigualdade++; 
+        }
+    }
+    return contadorigualdade;
+}
+
+void compare (int neuronioletra[], int classeA, int classeB, int classeC, int classeD, int classeE, int classeF, int classeG, int classeH, int classeI, int classeJ, int classeK, int classeL, int classeM, int classeN, int classeO, int classeP, int classeQ, int classeR, int classeS, int classeT, int classeU, int classeV, int classeW, int classeX, int classeY, int classeZ){
+    //Comapra uma letra com todas da base de dados
+  int vet[26];
+  vet[0]=comparadisc(neuronioletra,classeA);
+  vet[1]=comparadisc(neuronioletra,classeB);
+  vet[2]=comparadisc(neuronioletra,classeC);
+  vet[3]=comparadisc(neuronioletra,classeD);
+  vet[4]=comparadisc(neuronioletra,classeE);
+  vet[5]=comparadisc(neuronioletra,classeF);
+  vet[6]=comparadisc(neuronioletra,classeG);
+  vet[7]=comparadisc(neuronioletra,classeH);
+  vet[8]=comparadisc(neuronioletra,classeI);
+  vet[9]=comparadisc(neuronioletra,classeJ);
+  vet[10]=comparadisc(neuronioletra,classeK);
+  vet[11]=comparadisc(neuronioletra,classeL);
+  vet[12]=comparadisc(neuronioletra,classeM);
+  vet[13]=comparadisc(neuronioletra,classeN);
+  vet[14]=comparadisc(neuronioletra,classeO);
+  vet[15]=comparadisc(neuronioletra,classeP);
+  vet[16]=comparadisc(neuronioletra,classeQ);
+  vet[17]=comparadisc(neuronioletra,classeR);
+  vet[18]=comparadisc(neuronioletra,classeS);
+  vet[19]=comparadisc(neuronioletra,classeT);
+  vet[20]=comparadisc(neuronioletra,classeU);
+  vet[21]=comparadisc(neuronioletra,classeV);
+  vet[22]=comparadisc(neuronioletra,classeW);
+  vet[23]=comparadisc(neuronioletra,classeX);
+  vet[24]=comparadisc(neuronioletra,classeY);
+  vet[25]=comparadisc(neuronioletra,classeZ);
+
+  for(int i=0;i<26; i++){
+      printf("%d", vet[i]);
+  }
+  
+  int i=0, j,maior=vet[0],aux,letra=25,vencedora; 
+  //inicializo maior com o valor do vet na posição zero e assim posso apagar a comparação inicial dentro do for
+  for (int i = 0; i <  26; ++i) {            
+        if(vet[i] > maior){
+            maior = vet[i];
+            letra  = i;
+        }  
+  } 
+
+  for(i=0;i<26;i++){
+    for(j=i+1;j<26;j++){
+      if(vet[i]>vet[j]){
+        aux=vet[i];
+        vet[i]=vet[j];
+        vet[j]=aux;
+      }
+    }
+  }//se tiver um numero maior ele vai ser printado
+
+  if(vet[25]>vet[24]){
+    vencedora=65+letra;
+    printf("\nA letra pertence a classe %c\n",vencedora);
+  }//em caso de empate avisa que nao foi possivel determinar
+  if(vet[25]==vet[24]){
+    printf("\nNao foi possivel determinar a classe\n");
+  }
+}
